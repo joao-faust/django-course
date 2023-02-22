9364e4468fdb76613fa904e845c500cbeb33d358
joao-faust <joaovsf2004@gmail.com>
2023-02-21 21:44:35 -0300

    Add class 9

--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from contas.views import home, listagem, nova_transacao
+from contas.views import home, listagem, nova_transacao, update
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listagem, name='url_listagem'),
-    path('home/', home),
+    path('home/', home, name='url_home'),
     path('nova/', nova_transacao, name='url_nova'),
+    path('update/<int:pk>/', update, name='url_update'),
 ]
